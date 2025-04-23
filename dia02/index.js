2ab756ca6687ba58675fead3db4ad16ff8ae01da
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-04-23 21:57:42 +0200

    Initial setup – re-learning the basics

--- dia02/index.js
+++ dia02/index.js
@@ -1,2 +1,11 @@
-console.log("Hello World");
+// Format CommonJS to import libraries
+//const cowsay = require("cowsay");
 
+//Format ES6 to import libraries
+import cowsay from "cowsay";
+
+console.log(cowsay.say({
+    text: "I'm a moooodule",
+    e: "oOo",
+    T: "U ",
+}))
