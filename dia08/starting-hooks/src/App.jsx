ed6e71c480b66ae1c5bf51895aa32a122113f76b
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-05-13 20:01:44 +0200

    feat: add preview thumbnails for next and previous images

--- dia08/starting-hooks/src/App.jsx
+++ dia08/starting-hooks/src/App.jsx
@@ -118,7 +118,6 @@ const Form = () => {
 
 const Gallery = () => {
   const [position, setPosition] = useState(0)
-
   const handlePrevious = () => {
     if (position - 1 === -1) {
       setPosition(imageGallery.length - 1)
@@ -130,17 +129,27 @@ const Gallery = () => {
   const handleNext = () => {
     if (position + 1 === imageGallery.length) {
       setPosition(0)
+
     } else {
       setPosition(prevState => prevState + 1)
     }
   }
+  const prevImg = (position === 0) ? imageGallery.length - 1 : position -1
+  const nextImg = (position === imageGallery.length -1 ) ? 0 : position + 1
   return (
     <div className='movie-section'>
       <img src={imageGallery[position].url} className='movie-img' />
       <h2>{imageGallery[position].title}</h2>
-
-      <button onClick={handlePrevious}>Prev</button>
-      <button onClick={handleNext}>Next</button>
+      <div className='container-buttons'>
+        <div className='img-buttons'>
+          <img src={imageGallery[prevImg].url} />
+          <button onClick={handlePrevious}>Prev</button>
+        </div>
+        <div className='img-buttons'>
+          <button onClick={handleNext}>Next</button>
+          <img src={imageGallery[nextImg].url} />
+        </div>
+      </div>
     </div>
   )
 }
