a1c3c4658effa17361fe523510aa5e0b22cf23bc
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-05-14 19:35:03 +0200

    Add basic timer component with start, stop, and reset functionality

--- dia08/starting-hooks/src/App.jsx
+++ dia08/starting-hooks/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <Gallery />
       <Game randomnumber={randomnumber}/>
       <TextArea />
+      <Timer />
     </>
   )
 }
@@ -195,6 +196,44 @@ const TextArea = () => {
       <textarea cols="30" rows="10" value={userText} onChange={handleChange}></textarea>
       <p>Words: {words}</p>
       <p>Characters: {char}</p>
+    </div>
+  )
+}
+
+const Timer = () => {
+  const [time, setTime] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [intervalId, setIntervalId] = useState(null)
+
+  const handleStart = () => {
+    if (isActive) return;
+
+    setIsActive(true)
+    const id = setInterval(() => {
+      setTime((prev) => {
+        const next = prev + 0.1;
+        return Math.round(next * 100) / 100;
+      });
+    }, 100);
+    setIntervalId(id)
+  }
+
+  const handleStop = () => {
+    clearInterval(intervalId)
+    setIsActive(false)
+  }
+
+  const handleReset = () => {
+    setTime(0)
+  }
+
+  return(
+    <div className='movie-section'>
+      <h1>Timer</h1>
+      <h2>{time}</h2>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
