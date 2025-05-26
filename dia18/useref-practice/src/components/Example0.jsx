9bf556290798c1e546f6324e14bff79ba4617cb4
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-05-26 21:24:12 +0200

    feat: focus and update input value using useRef

--- dia18/useref-practice/src/components/Example0.jsx
+++ dia18/useref-practice/src/components/Example0.jsx
@@ -6,12 +6,22 @@ const Example0 = () => {
     useEffect (() => {
         counter2.current = counter2.current + 1
     })
+
+    const inputRef = useRef();
+    const handleAction = () => {
+        inputRef.current.focus();
+        inputRef.current.value = "Hello: " + counter2.current;
+    }
     return ( 
         <div>
             <input type="text" value={name} onChange={e => setName(e.target.value)}/>
             <h3>{name}</h3>
 
             <p>Render: {counter2.current}</p>
+
+
+            <input type="text" ref={inputRef} value={name}/>
+            <button onClick={handleAction}>Action</button>
         </div>
     );
 }
