99e3301e0f3ba64b245a5d9a2963504649ba3328
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-05-28 21:55:10 +0200

    chore: practice dynamic routing

--- dia20/backend1/index.js
+++ dia20/backend1/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-
 app.get('/', (req, res) => {
     res.send("Hello world")
 });
@@ -10,6 +9,10 @@ app.get('/contact', (req, res) => {
     res.send('Contact')
 })
 
+app.get("/contact/:id", (req, res) => {
+    const idContact = req.params.id;
+    res.send(`Contact id: ${idContact}`)
+})
 app.listen(3000, () => {
     console.log("Listening in port 3000")
 });
