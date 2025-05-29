4dfeba09a6ce312bd1b45f3bf62d2d1739422a9b
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-05-29 21:58:23 +0200

    feat(api): add GET and POST routes for tasks

--- dia21/backend2/index.js
+++ dia21/backend2/index.js
@@ -12,6 +12,31 @@ app.get("/", (req, res) => {
     res.send("Welcome")
 })
 
+const tasks = [
+    { id: 1, text: "Learn Express", isComplete: false },
+    { id: 2, text: 'Create an API Restfull', isComplete: false }
+];
+
+app.get('/api/v1/tasks', (req, res) => {
+    res.json(tasks)
+})
+
+app.get('/api/v1/tasks/:id', (req, res) => {
+    const { id } = req.params
+    const task = tasks.find(t => t.id === Number(id));
+
+    if (!task) {
+        return res.status(404).json({msg: 'Task not found'});
+    }
+
+    res.status(200).json(task);
+})
+
+app.post('/api/v1/tasks', (req, res) => {
+    const { text, isComplete } = req.body
+    tasks.push({id: tasks.length + 1, text: text, isComplete: isComplete})
+    res.status(201).json(tasks)
+})
 
 app.listen(3000, () => {
     console.log('Listening in port 3000')
