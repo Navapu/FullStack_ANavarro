e8b3c42b450d477662165f5f937476c70f30593e
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-06-03 21:16:54 +0200

    feat(middleware): add global error handler and 404 route catcher

--- dia22/backend3/index.js
+++ dia22/backend3/index.js
@@ -3,12 +3,14 @@ import {logMiddleware} from './middleware/logger.middleware.js';
 import { logs } from './db/logs.js';
 import { uniqueIDWare } from './middleware/uniqueid.middleware.js';
 import { validateMiddleware } from './middleware/validate.middleware.js';
+import { errorMiddleware, notFoundHandler } from './middleware/error.midddleware.js';
 const app = express();
 
 
 app.use(express.json())
 app.use(uniqueIDWare)
 app.use(logMiddleware);
+
 app.get("/", (req, res) => {
     res.send('Middleware Playground')
 })
@@ -21,6 +23,17 @@ app.post('/users/add', validateMiddleware, (req, res) => {
     res.json({name: name, age: age})
 })
 
+app.get('/checkerror', (req, res, next) => {
+    try{
+        console.log("Checking the error message")
+        const num =3;
+        num = 2;
+    }catch(error){
+        next(error)
+    }
+})
+app.use(notFoundHandler);
+app.use(errorMiddleware);
 app.listen(3000, () => {
     console.log('Listening in port 3000')
 })
