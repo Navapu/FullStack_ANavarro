5289759d0d25438c62ed788869606cba62f14e9e
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-06-11 21:09:33 +0200

    chore(env): test loading of private and public environment variables

--- dia29/catalog-nextjs/src/app/page.js
+++ dia29/catalog-nextjs/src/app/page.js
@@ -2,8 +2,21 @@ import Counter from "@/components/Counter";
 import Image from "next/image";
 
 export default function Home() {
+
+  // If is "use client" this two won't be displayed
+  const user= process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+
+  const public_user = process.env.NEXT_PUBLIC_USER;
+  const BACKEND_API = process.env.NEXT_PUBLIC_BACKEND_API;
   return (
     <div className="items-center justify-center h-full flex flex-col">
+      <p>
+        User: {user} <br />
+        Pass: {password} <br />
+        Public User: {public_user} <br />
+        Backend API: {BACKEND_API}
+      </p>
       <Counter />
     </div>
   );
