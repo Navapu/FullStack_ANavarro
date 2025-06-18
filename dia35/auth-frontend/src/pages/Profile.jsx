189f04c99ac1cdbbd1b14877489f13f7db99b5da
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-06-18 22:00:28 +0200

    feat(auth): add protected Profile page with user fetch via token
    - Created Profile page that fetches user data from the protected /auth/me endpoint using the JWT token stored in localStorage.
    - Includes basic error handling and UI structure for displaying user information and logout button.

--- dia35/auth-frontend/src/pages/Profile.jsx
+++ dia35/auth-frontend/src/pages/Profile.jsx
@@ -1,7 +1,45 @@
+import { useState, useEffect} from "react";
 const Profile = () => {
+    const BACKEND_API = import.meta.env.VITE_BACKEND_API;
+
+    const [error, setError] = useState(null);
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        fetchUser();
+    }, [])
+
+    const fetchUser = async () => {
+        try{
+            const token = localStorage.getItem('token');
+            const response = await fetch(`${BACKEND_API}/auth/me`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            const responseData = await response.json();
+
+            if(!response.ok || responseData.status === "error"){
+                throw new Error(responseData.msg);
+            }
+            console.log(responseData)
+            setUser(responseData)
+        }catch(e){
+            setError(e.message)
+        }
+    }
+    
     return ( 
         <div>
-            <h1 className="text-2xl font-bold mb-4">Im' Profile</h1>
+            <h1 className="text-2xl font-bold mb-4">Im' Profile (protected route)</h1>
+
+            <div>
+                {/* <p>Welcome: <b>{name}</b></p>
+                <p>Email: <b>{email}</b></p> */}
+                <button>Logout</button>
+            </div>
+
+            {error && <p className="text-red-400">{error}</p>}
         </div>
      );
 }
