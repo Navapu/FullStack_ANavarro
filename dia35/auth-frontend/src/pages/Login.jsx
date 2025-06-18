2a76a3e12c1742c1e597a273b4af8ee22ce46bfa
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-06-18 21:45:26 +0200

    feat(auth): add user registration form with backend integration
    - Implemented a registration form in the React frontend that connects to the Express API.
    - Includes form submission using fetch, JSON payload, and error handling from API responses.

---
+++ dia35/auth-frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return ( 
+        <div>
+            <h1 className="text-2xl font-bold mb-4">Im' Login</h1>
+        </div>
+     );
+}
+ 
+export default Login;
---
+++ dia35/auth-frontend/src/pages/Register.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import {useNavigate} from 'react-router';
+const Register = () => {
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+        name: ""
+    })
+    const navigate = useNavigate();
+    const [error, setError] = useState(null);
+    const handleChange = (e) => {
+        setForm(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+    const BACKEND_API = import.meta.env.VITE_BACKEND_API;
+    const {email, name, password} = form;
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await fetch(`${BACKEND_API}/auth/register`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(form)
+            });
+            const responseData = await response.json();
+
+            if(!response.ok || responseData.status === "error"){
+                throw new Error(responseData.msg)
+            }
+            console.log(responseData)
+
+            localStorage.setItem('token', responseData.data.token);
+            setError(null)
+            navigate("/profile")
+        }catch(e){
+            setError(e.message)
+        }
+    }
+    return ( 
+        <div>
+            <h1 className="text-2xl font-bold mb-4">I'm Register</h1>
+
+            <form onSubmit={handleSubmit} className="space-y-4" method="POST">
+                <input type="email" value={email} name="email" placeholder="Email" onChange={handleChange} className="border border-gray-300 rounded p-2 w-full"/>
+                <input type="password" value={password} name="password" placeholder="Password" onChange={handleChange} className="border border-gray-300 rounded p-2 w-full"/>
+                <input type="text" value={name} name="name" placeholder="Name" onChange={handleChange} className="border border-gray-300 rounded p-2 w-full"/>
+                <button type="submit">Register</button>
+            </form>
+
+            {error && <p className="text-red-400">{error}</p>}
+        </div>
+     );
+}
+ 
+export default Register;
