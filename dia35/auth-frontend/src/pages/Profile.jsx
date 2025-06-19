78dc7f67de33fa85640983372fe89107fe5865d7
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-06-19 19:33:58 +0200

    feat(auth): add logout functionality
    - Added working logout button in Profile page
    - Clears token from localStorage and resets user state
    - Redirects to login page after logout

--- dia35/auth-frontend/src/pages/Profile.jsx
+++ dia35/auth-frontend/src/pages/Profile.jsx
@@ -1,7 +1,9 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router';
+
 const Profile = () => {
     const BACKEND_API = import.meta.env.VITE_BACKEND_API;
-
+    const navigate = useNavigate();
     const [error, setError] = useState(null);
     const [user, setUser] = useState(null);
     useEffect(() => {
@@ -9,7 +11,7 @@ const Profile = () => {
     }, [])
 
     const fetchUser = async () => {
-        try{
+        try {
             const token = localStorage.getItem('token');
             const response = await fetch(`${BACKEND_API}/auth/me`, {
                 headers: {
@@ -19,29 +21,36 @@ const Profile = () => {
 
             const responseData = await response.json();
 
-            if(!response.ok || responseData.status === "error"){
+            if (!response.ok || responseData.status === "error") {
                 throw new Error(responseData.msg);
             }
             setUser(responseData.data);
-        }catch(e){
+            setError(null)
+        } catch (e) {
             setError(e.message)
         }
     }
     const name = user?.name;
     const email = user?.email;
-    return ( 
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        setUser(null);
+        navigate("/login");
+    }
+    return (
         <div>
             <h1 className="text-2xl font-bold mb-4">Im' Profile (protected route)</h1>
 
             <div>
                 <p>Welcome: <b>{name}</b></p>
                 <p>Email: <b>{email}</b></p>
-                <button>Logout</button>
+
+                <button onClick={handleLogout} className="m-3">Logout</button>
             </div>
 
             {error && <p className="text-red-400">{error}</p>}
         </div>
-     );
+    );
 }
- 
+
 export default Profile;
