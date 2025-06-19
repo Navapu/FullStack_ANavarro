8c72876da00f757107fa4c2c0266ec39ffae0085
andreamiguel <100772082+Navapu@users.noreply.github.com>
2025-06-19 19:18:05 +0200

    feat(auth): implement login page with form submission and token storage

--- dia35/auth-frontend/src/pages/Login.jsx
+++ dia35/auth-frontend/src/pages/Login.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import {useNavigate} from 'react-router';
 const Login = () => {
+    const [form, setForm] = useState({
+        email: "",
+        password: ""    
+    })
+    const navigate = useNavigate();
+    const [error, setError] = useState(null);
+    const handleChange = (e) => {
+        setForm(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+    const BACKEND_API = import.meta.env.VITE_BACKEND_API;
+    const {email, password} = form;
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await fetch(`${BACKEND_API}/auth/login`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(form)
+            });
+            const responseData = await response.json();
+
+            if(!response.ok || responseData.status === "error"){
+                throw new Error(responseData.msg)
+            }
+            console.log(responseData)
+
+            localStorage.setItem('token', responseData.data.token);
+            setError(null)
+            navigate("/profile")
+        }catch(e){
+            setError(e.message)
+        }
+    }
     return ( 
         <div>
-            <h1 className="text-2xl font-bold mb-4">Im' Login</h1>
+            <h1 className="text-2xl font-bold mb-4">I'm Login</h1>
+
+            <form onSubmit={handleSubmit} className="space-y-4" method="POST">
+                <input type="email" value={email} name="email" placeholder="Email" onChange={handleChange} className="border border-gray-300 rounded p-2 w-full"/>
+                <input type="password" value={password} name="password" placeholder="Password" onChange={handleChange} className="border border-gray-300 rounded p-2 w-full"/>
+                <button type="submit">Login</button>
+            </form>
+
+            {error && <p className="text-red-400">{error}</p>}
         </div>
      );
 }
